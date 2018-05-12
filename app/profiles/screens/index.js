42b11f0c1c1e572fe49de59badc9a921e7bd0f0b
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-05-13 06:04:06 +0700

    adding header part done

--- app/profiles/screens/index.js
+++ app/profiles/screens/index.js
@@ -2,37 +2,46 @@ import React, { Component } from 'react'
 import {
   Container, Content, Card,
   CardItem, Body, Text,
-  Thumbnail //add thumbnail on import
+  Thumbnail
 } from 'native-base'
-import { View, StyleSheet } from 'react-native'
+//#1. import Image, and Dimensions from RN
+import { View, StyleSheet, Image, Dimensions } from 'react-native'
+
+//#2. Get the dimensions width of the android screen
+const width = Dimensions.get('window').width
 
 export default class Profiles extends Component{
 
-  //create the state
   state = {
     data: {
       name: 'Ega Radiegtya',
       position: 'Founder of DumbWays.id',
       education: 'Satya Wacana Christian University',
       summary: "I am React Native and Django Developer. I love to share about business and technology.",
-      //add your profile picture url
-      profilePicture: "https://develop.backendless.com/221BAB21-F149-D2B2-FF55-B2DD8ECDFE00/console/flkehivadqphgmtbycrjozdigqkrwvpnfezw/files/view/images/me_animated.png"
+      profilePicture: "https://develop.backendless.com/221BAB21-F149-D2B2-FF55-B2DD8ECDFE00/console/flkehivadqphgmtbycrjozdigqkrwvpnfezw/files/view/images/me_animated.png",
+      //#3. Add bgPicture on state
+      bgPicture: "https://amazingpict.com/wp-content/uploads/2017/03/flat-1600x900-forest-deer-4k-5k-iphone-wallpaper-abstract-11925.jpg"
     }
   }
 
   render(){
-    //add profilePicture variable
-    const {name, position, education, summary, profilePicture} = this.state.data
+    //#4. add bgPicture on list spread var
+    const {name, position, education, summary, profilePicture, bgPicture} = this.state.data
     return (
         <Container>
-          {/* Add Thumbnail with uri from profilePicture */}
+          {/* #5. call bgPicture component */}
+          <View style={styles.bgPictureContainer}>
+            <Image
+              source={{uri: bgPicture}}
+              style={styles.bgPicture}
+            />
+          </View>
           <View style={styles.profilePicture}>
             <Thumbnail
               large
               source={{uri: profilePicture}}
             />
           </View>
-          {/* adding marginTop on content to give space for bgPicture later */}
           <Content style={{marginTop: 120}}>
             <Card>
               <CardItem>
@@ -54,8 +63,17 @@ export default class Profiles extends Component{
 
 }
 
-//create styles const to make our styles more readable
 const styles = StyleSheet.create({
+  //#6. adding bgPictureContainer styling, use zIndex 0 to make it at the very bottom of screen
+  bgPictureContainer: {
+    position: 'absolute',
+    zIndex: 0,
+  },
+  //#7. adding bgPicture styling, with width from android dimensions and height hard coded 
+  bgPicture: {
+    width: width,
+    height: 200
+  },
   profilePicture: {
     paddingTop: 70,
     position: 'absolute',
