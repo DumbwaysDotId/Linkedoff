fb66d4b8933440bb4be14906e2d9a6fd6a1022e8
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-05-13 07:51:34 +0700

    adding sub 2 at part 4

--- app/profiles/screens/index.js
+++ app/profiles/screens/index.js
@@ -4,32 +4,43 @@ import {
   CardItem, Body, Text,
   Thumbnail
 } from 'native-base'
-//#1. import Image, and Dimensions from RN
 import { View, StyleSheet, Image, Dimensions } from 'react-native'
+import axios from 'axios'
 
-//#2. Get the dimensions width of the android screen
 const width = Dimensions.get('window').width
 
 export default class Profiles extends Component{
 
   state = {
-    data: {
-      name: 'Ega Radiegtya',
-      position: 'Founder of DumbWays.id',
-      education: 'Satya Wacana Christian University',
-      summary: "I am React Native and Django Developer. I love to share about business and technology.",
-      profilePicture: "https://develop.backendless.com/221BAB21-F149-D2B2-FF55-B2DD8ECDFE00/console/flkehivadqphgmtbycrjozdigqkrwvpnfezw/files/view/images/me_animated.png",
-      //#3. Add bgPicture on state
-      bgPicture: "https://amazingpict.com/wp-content/uploads/2017/03/flat-1600x900-forest-deer-4k-5k-iphone-wallpaper-abstract-11925.jpg"
-    }
+    data: {}
   }
 
+  allData(){
+    const url = `https://api.backendless.com/221BAB21-F149-D2B2-FF55-B2DD8ECDFE00/31717453-DFFE-7469-FF91-D1EAC0C16700/data/profiles/564DD7D4-4F12-B56A-FFD5-1E4B0E7D9D00?loadRelations=experiences%2Ceducations`
+    axios.get(url).then((result)=>{
+      this.setState({
+        data: result.data
+      })
+    })
+  }
+
+  componentDidMount(){
+    this.allData()
+  }
+
+
   render(){
-    //#4. add bgPicture on list spread var
-    const {name, position, education, summary, profilePicture, bgPicture} = this.state.data
+    // const {name, position, education, summary, profilePicture, bgPicture} = this.state.data
+    const {name, experiences, educations, summary, profilePicture, bgPicture} = this.state.data
+
+    const latestExperience = experiences? experiences[0]: null
+    const position = latestExperience? `${latestExperience.position} at ${latestExperience.companyName}`: '-'
+
+    const latestEducation = educations? educations[0]: null
+    const education = latestEducation? latestEducation.school: '-'
+
     return (
         <Container>
-          {/* #5. call bgPicture component */}
           <View style={styles.bgPictureContainer}>
             <Image
               source={{uri: bgPicture}}
@@ -47,7 +58,7 @@ export default class Profiles extends Component{
               <CardItem>
                 <Body style={styles.centeredBody}>
                   <Text style={styles.nameTitle}>{name}</Text>
-                  <Text style={styles.position}>{position}</Text>
+                  <Text style={styles.experiences}>{position}</Text>
                   <Text style={styles.education}>{education}</Text>
 
                   <Text style={styles.summary}>
@@ -64,12 +75,10 @@ export default class Profiles extends Component{
 }
 
 const styles = StyleSheet.create({
-  //#6. adding bgPictureContainer styling, use zIndex 0 to make it at the very bottom of screen
   bgPictureContainer: {
     position: 'absolute',
     zIndex: 0,
   },
-  //#7. adding bgPicture styling, with width from android dimensions and height hard coded 
   bgPicture: {
     width: width,
     height: 200
